e47b70b9751423c99350fbbe481aadb592fa8537
Michael Hacker <hack1973@swbell.net>
2021-09-08 04:40:14 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(DigitSumChallenge.sumDigits(125));
+        System.out.println(DigitSumChallenge.sumDigits(-125));
+        System.out.println(DigitSumChallenge.sumDigits(4));
+        System.out.println(DigitSumChallenge.sumDigits(32123));
+    }
+}
